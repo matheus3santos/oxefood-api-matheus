3757d3dd160800c78f0762a40001d4645a1e64fc
Matinhas <matheusvictor007@gmail.com>
2024-03-17 19:22:02 -0300

    Refazendo projeto

--- README.md
+++
@@ -1,1 +0,0 @@
-# oxefood-api-matheus
--- src/java/br/com/ifpe/oxefood/modelo/cliente/ClienteRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.ifpe.oxefood.modelo.cliente;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-
-  
-}
