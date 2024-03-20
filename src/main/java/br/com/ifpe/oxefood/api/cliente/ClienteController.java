4f25f29458ea9297657709daca3ea24bc3d9c9e7
aluno <aluno@ifpe>
2024-03-20 20:58:06 -0300

    MetodoGET - Cliente

--- src/main/java/br/com/ifpe/oxefood/api/cliente/ClienteController.java
+++ src/main/java/br/com/ifpe/oxefood/api/cliente/ClienteController.java
@@ -1,5 +1,7 @@
 package br.com.ifpe.oxefood.api.cliente;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,20 +13,32 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.ifpe.oxefood.modelo.cliente.Cliente;
 import br.com.ifpe.oxefood.modelo.cliente.ClienteService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/api/cliente")
 @CrossOrigin
 public class ClienteController {
 
-   @Autowired
-   private ClienteService clienteService;
+    @Autowired
+    private ClienteService clienteService;
 
-   @PostMapping
-   public ResponseEntity<Cliente> save(@RequestBody ClienteRequest request) {
+    @PostMapping
+    public ResponseEntity<Cliente> save(@RequestBody ClienteRequest request) {
 
-       Cliente cliente = clienteService.save(request.build());
-       return new ResponseEntity<Cliente>(cliente, HttpStatus.CREATED);
+        Cliente cliente = clienteService.save(request.build());
+        return new ResponseEntity<Cliente>(cliente, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public List<Cliente> listarTodos() {
+        return clienteService.listarTodos();
+    }
+
+   @GetMapping("/{id}")
+   public Cliente obterPorID(@PathVariable Long id) {
+       return  clienteService.obterPorID(id);
    }
-   
+
 }
--- src/main/java/br/com/ifpe/oxefood/modelo/cliente/ClienteService.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/cliente/ClienteService.java
@@ -1,6 +1,11 @@
 package br.com.ifpe.oxefood.modelo.cliente;
+
+
+//Caso a função não funcione importar ela manualmente
+import java.util.List;
 import jakarta.transaction.Transactional;
 import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,4 +24,13 @@ public class ClienteService {
         return repository.save(cliente);
     }
 
+    public List<Cliente> listarTodos() {
+
+        return repository.findAll();
+    }
+
+    public Cliente obterPorID(Long id) {
+
+        return repository.findById(id).get();
+    }
 }
